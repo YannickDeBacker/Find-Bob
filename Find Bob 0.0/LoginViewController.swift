2d2a7ef824914dc1cc1ec760d18eab4ad2f9be7e
Yannick <y.deback@hotmail.com>
2020-03-19 20:41:12 +0100

    GitHub working and starting on Login

---
+++ Find Bob 0.0/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Find Bob 0.0
+//
+//  Created by Yannick De Backer on 19/03/2020.
+//  Copyright © 2020 Yannick De Backer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var upperButton: UIButton!
+    @IBOutlet weak var lowerButton: UIButton!
+     
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
